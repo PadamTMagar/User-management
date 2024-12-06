78ec1391e4fecca64f9068e609987f91b025d0ed
PadamTMagar <padamtmgr567@gmail.com>
2024-12-07 02:22:57 +0545

    first commit

---
+++ conn.php
@@ -0,0 +1,4 @@
+<?php
+$conn = mysqli_connect("192.168.1.65","root","","user_managagement");
+if(!$conn) die("Database connection is failed");
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+include("conn.php");
+?>
+<div class="form-box">
+    <h1>Login User</h1>
+    <form action="" method="POST" name="userform"> <!-- action attribute specifies where to send the form data when a form is submitted -->
+    <label for="name">Username/Email</label>
+        <input type="text" id="uname" name="username" value="">
+        <span></span>
+    </div>
+    <div class="form-item">
+        <label for="pwd">Password</label>
+        <input type="password" id="pwd" name="password" value="">
+        <span></span>
+    </div>
+
+    <button type="submit" name="submit" class="btn">Login</button>
+    </form> 
+    <div class="form-item">
+    
+</div>
