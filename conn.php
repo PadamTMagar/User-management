4cc5186da37f72b98d90058e7afd02cb96643dc1
PadamTMagar <padamtmgr567@gmail.com>
2024-12-07 02:47:29 +0545

    update

--- conn.php
+++ conn.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect("192.168.1.65","root","","user_managagement");
+$conn = mysqli_connect("192.168.1.65","root","","user_management" , "3306");
 if(!$conn) die("Database connection is failed");
 ?>
